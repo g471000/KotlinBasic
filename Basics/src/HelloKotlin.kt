b0a80127c2f8e05e51dc74ddfef61d5a10a5bd3e
JIEUN CHON <g471000@gmail.com>
2021-01-23 12:57:15 +0900

    use arrayOf

--- Basics/src/HelloKotlin.kt
+++ Basics/src/HelloKotlin.kt
@@ -3,5 +3,14 @@
  * Also, you do not need to type long long println method as like Java
  */
 fun main() {
-    println("Hello Kotlin!")
+    val heizeAlbums = arrayOf("Lyricist", "Late Autumn", "Fine", "Wind", "You" +
+            " Are Cloudy", "And July")
+    println(heizeAlbums.size)
+    heizeAlbums.forEach { album ->
+        println("Heize's $album")
+    }
+
+    heizeAlbums.forEachIndexed { index, album ->
+        println("Heize album $index: $album")
+    }
 }
